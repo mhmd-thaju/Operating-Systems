b362282ce73887ed811b8058b29096df72d8d251
Muhammed Thajudheen <thajupm@gmail.com>
2025-02-11 15:26:20 +0530

    FIFS

--- FCFS/FIFS.cpp
+++ FCFS/FIFS.cpp
@@ -3,7 +3,6 @@
 using namespace std;
 
 int n;
-
 class Node {
     public :
         int pid,at,bt;
@@ -11,36 +10,52 @@ class Node {
             this->at = at;
             this->pid = pid;
             this->bt= bt;
-        }
-        
+        }        
 };
 
 class FCFS{
     private:
         queue <Node> processQ;
-        queue 
     public:
         void addProcess(int pid,int at,int bt){
             Node newprocess(pid,at,bt);
             processQ.push(newprocess);
         }
         void print(){
+            queue <Node> temp = processQ;
             cout<<"PID\tAT\tBT\n"; 
             for(int i=0;i<n;i++){
-                Node p = processQ.front();
-                processQ.pop();
+                Node p = temp.front();
+                temp.pop();
                 cout<<p.pid<<"\t"<<p.at<<" \t"<<p.bt<<"  \n"; 
             }
         }
-        void cwt(){
-            int temp,,awt,ar[n];
-            for(int i=0;i<n;i++){
-                Node p = processQ.front();
-                process
+        void ganttchart(){
+            int temp=0,awt = 0,b[n+1];
+            cout<<endl<<endl<<"_______Gnatt Chart________"<<endl;
+            queue <Node> tmp = processQ;
+            b[0]=temp;
+            for(int i=0;i<n;++i){
+                Node p = tmp.front();
+                cout<<"|\tP"<<p.pid<<"\t";
+                if(temp == p.at)
+                {
+                	temp += p.bt;
+				}
+				else if(temp > p.at ){
+					awt += temp - p.at;
+					temp += p.bt;
+				}
+                b[i+1]=temp;
+                tmp.pop();    
             }
-        }
+            cout<<"|\t\n";
+            for (int i=0;i<n+1;++i)    cout<<b[i]<<"\t  \t";
+            cout<<"\n\nTotal turnaround time : "<<temp<<endl;
+            cout<<"Total Waiting time : "<<awt<<endl;
+            cout<<"Average waiting time : "<<awt/n<<endl;
+        }      
 };
-
 
 int main(){
     FCFS scheduler;
@@ -52,8 +67,6 @@ int main(){
         cin>>at>>bt;
         scheduler.addProcess(i+1,at,bt);
     }
-
     scheduler.print();
-    scheduler.cwt();
-
+    scheduler.ganttchart();
 }
