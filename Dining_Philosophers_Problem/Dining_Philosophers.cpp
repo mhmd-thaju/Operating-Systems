d34693bbb5473ae878d80bb3f80d4499c17f4fcc
Muhammed Thajudheen <thajupm@gmail.com>
2025-03-04 14:29:40 +0530

    Create Dining_Philosophers.cpp

---
+++ Dining_Philosophers_Problem/Dining_Philosophers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+const int NUM_PHILOSOPHERS = 5;
+mutex forks[NUM_PHILOSOPHERS];
+
+void philosopher(int i){
+    cout<<"Philosopher "<<i+1<<" is thinking "<<endl;
+    this_thread::sleep_for(chrono::seconds(1));
+    int leftfork = i;
+    int rightfork =(i+1)% NUM_PHILOSOPHERS;
+    unique_lock <mutex> lockleft(forks[leftfork]);
+    cout<<"Philospher "<<i+1<<" picked left fork "<<leftfork<<endl;
+    unique_lock <mutex> lockright(forks[rightfork]);
+    cout<<"Philospher "<<i+1<<" picked right fork "<<rightfork<<endl;
+    cout<<"Philosopher "<<i+1<<" is eating"<<endl;
+    this_thread::sleep_for(chrono::seconds(2));
+    cout<<"Philosopher "<<i+1<<" finished eating and is releasing forks"<<endl;
+}
+
+int main(){
+    vector <thread> philosophers;
+    for(int i=0;i<NUM_PHILOSOPHERS;++i){
+        philosophers.push_back(thread(philosopher,i));
+    }
+    for(auto&t : philosophers){
+        t.join();
+    }
+    return 0;
+}
